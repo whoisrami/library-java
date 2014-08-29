74059791411a6f7bc09e2ef48cc397e956ffd8a3
Yang, Alex <alex.yang@cbsinteractive.com>
2014-08-29 02:24:45 -0700

    linked_lists: added delete and displayList to implementation

--- Linked_Lists/linkedListImplementation.java
+++ Linked_Lists/linkedListImplementation.java
@@ -10,6 +10,10 @@ class linkedListImplementation {
     head.appendToTail(4);
     head.appendToTail(9);
     head.appendToTail(2);
+    head.displayList();
+    head = head.deleteNode(head, 3);
+    head = head.deleteNode(head, 2);
+    head.displayList();
   } 
 }
 
@@ -18,7 +22,6 @@ class Node {
   int data;
 
   public Node (int data) {
-    System.out.println(data);
     this.data = data;
   }
 
@@ -26,19 +29,50 @@ class Node {
   void appendToTail (int data) {
     Node tail = new Node(data);
     Node n = this;
-    while(n.next != null){
-      System.out.println("next:" + n.next.getData());
+    while (n.next != null) {
       n = n.next;
     }
     n.next = tail;
   }
 
-  //TODO: implement delete node
-  
-  //TODO: implement insert node
+  //deletes a node from the linked list
+  Node deleteNode (Node head, int d) {
+    //if head is node to be removed, return head.next as the new head
+    if (head.data == d) {
+      System.out.println("deleting head at: " + d);
+      return head.next;
+    }    
+
+    Node n = head;
+    while (n.next != null) {
+      if (n.next.data == d) {
+        n.next = n.next.next;
+        return head; 
+      }
+      n = n.next;
+    }
+
+    return head;
+  }
 
   //returns data at given node
-  public int getData(){
+  public int getData() {
     return this.data;
   }
+
+  //prints out all nodes in linked list
+  void displayList(){
+    System.out.println("Displaying Linked List: ");
+    Node n = this;
+    while (n != null) {
+      System.out.print(n.data);
+      n = n.next;
+    }
+    System.out.println("\n");
+  }
 }
+
+
+
+
+
